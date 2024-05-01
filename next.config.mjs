ccf1ece9b9ed2d83624f98f498316276eb3bf272
KondRoll <kondroll.210@outlook.com>
2024-05-01 15:37:43 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: "dist",
+  output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true
+};
 
 export default nextConfig;
